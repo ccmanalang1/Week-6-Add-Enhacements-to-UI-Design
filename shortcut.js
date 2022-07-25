726e6c2c69b898ead4d9260ce1c7ba33ef89a04d
Charles Manalang <97075106+ccmanalang1@users.noreply.github.com>
2022-07-24 21:27:01 -0700

    Add files via upload

---
+++ shortcut.js
@@ -0,0 +1,7 @@
+index.getElementById('desktop-music').addEventListener('click', function(){
+    index.querySelector('.bg-modal').style.display = 'flex';
+});
+
+index.querySelector('.close').addEventListener('click', function(){
+    index.querySelector('.bg-modal').style.display='none';
+});
